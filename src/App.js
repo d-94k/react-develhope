59dafe73bab07d259de35e97ac04aeed187a0961
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-06 21:34:04 +0100

    corrected exercise

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import Hello from './Hello';
 
-class App extends React.Component {
+export default class App extends React.Component {
     render () {
         return (
             <div>
-                <Hello />
+              <Hello />
             </div>
         )
     }
--- src/Hello.js
+++ src/Hello.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default class Hello extends React.Component {
     render () {
         return <h1>Hello, World!</h1>
