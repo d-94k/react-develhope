d689f164484234d9db9e17ed1686ffcf194ab011
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-06 22:24:55 +0100

    completd exercise

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Age from "./Age";
 
 export default class Welcome extends React.Component {
     render () {
         return (
             <>
                 <p>Welcome, {this.props.name ? this.props.name : "guest"}!</p>
-                <p>Your age is {this.props.age}</p>
+                <Age age={this.props.age} />
             </>
         )
     }
