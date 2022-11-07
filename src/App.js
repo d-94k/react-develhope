8a41b1154ac085b6c5504f9522f405039a71d99a
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-07 14:18:03 +0100

    setup ex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ClickCounter from "./ClickCounter";
+import ClickTracker from "./ClickTracker";
 import Counter from "./Counter";
 import Hello from './Hello';
 import Welcome from "./Welcome";
@@ -12,6 +13,7 @@ export default class App extends React.Component {
               <Welcome name="Simon" age={10} />
               <Counter initVal={10} counterAmount={100} intervalAmount={300} />
               <ClickCounter />
+              <ClickTracker />
             </div>
         )
     }
---
+++ src/ClickTracker.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default class ClickTracker extends React.Component {
+    
+    clickReveal = (event) => {
+        console.log (event.target.innerHTML);
+    }
+
+    render () {
+        return (
+            <>
+                <h1>Which button was last clicked?</h1>
+                <button onClick={this.clickReveal}>First Button</button>    
+                <button onClick={this.clickReveal}>Second Button</button>
+                <button onClick={this.clickReveal}>Third Button</button>
+                <h1>The last button you clicked is... </h1>
+            </>
+        )
+    }
+}
