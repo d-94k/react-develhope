d469fecffbad2756d3fd4e155b542d3801da3afa
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-07 12:30:16 +0100

    removed componentDidMount

--- src/Counter.js
+++ src/Counter.js
@@ -3,13 +3,9 @@ import React from "react";
 export default class Counter extends React.Component {
     constructor (props) {
         super (props);
-        this.state = { counter: 0 }
-
-    }
-
-    componentDidMount () {
+        this.state = { counter: 0 };
         setInterval (() => {
-            this.setState (prevState => ({ counter: prevState.counter + 0.5}))
+            this.setState (prevState => ({ counter: prevState.counter + 1}))
         }, 1000)
     }
 
