c4fac95dd224f509a94e48da93dcdb4f4f2c72de
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-11 00:13:38 +0100

    completed exercise

--- src/Login.js
+++ src/Login.js
@@ -1,53 +1,71 @@
 import React from "react";
 
 export default class Login extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        username: "",
-        password: "",
-        remember: false
-      };
-    }
-  
-    inputHandler = (event) => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-  
-      this.setState({
-        [name]: value,
-      });
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      remember: false,
     };
-  
-    onLogin = () => {
-        console.log (this.state);
-    }
-  
-    render() {
-      return (
-        <>
-          <h3>Login.js Input:</h3>
-          <input
-            name="username"
-            value={this.state.username}
-            onChange={this.inputHandler}
-            placeholder="username"
-          />
-          <input
-            name="password"
-            value={this.state.password}
-            onChange={this.inputHandler}
-            placeholder="password"
-          />
-          <input
-            name="remember"
-            type="checkbox"
-            checked={this.state.remember}
-            onChange={this.inputHandler}
-          />
-          <button onClick={this.onLogin} disabled={this.state.username !== "" && this.state.password !== "" ? false : true}>Submit</button>
-        </>
-      );
-    }
   }
+
+  inputHandler = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onLogin = () => {
+    console.log(this.state);
+  };
+
+  onReset = () => {
+    this.setState({
+      username: "",
+      password: "",
+      remember: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h3>Login.js Input:</h3>
+        <input
+          name="username"
+          value={this.state.username}
+          onChange={this.inputHandler}
+          placeholder="username"
+        />
+        <input
+          name="password"
+          value={this.state.password}
+          onChange={this.inputHandler}
+          placeholder="password"
+        />
+        <input
+          name="remember"
+          type="checkbox"
+          checked={this.state.remember}
+          onChange={this.inputHandler}
+        />
+        <button
+          onClick={this.onLogin}
+          disabled={
+            this.state.username !== "" && this.state.password !== ""
+              ? false
+              : true
+          }
+        >
+          Submit
+        </button>
+        <button onClick={this.onReset}>Reset</button>
+      </>
+    );
+  }
+}
