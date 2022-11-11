70feb743ed51d7b9a5f078d8c9f46989494b09ea
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-11 12:36:56 +0100

    corrected

--- src/Login.js
+++ src/Login.js
@@ -1,53 +1,36 @@
 import React from "react";
 
 export default class Login extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        username: "",
-        password: "",
-        remember: false
-      };
+    constructor (props) {
+        super (props);
+        this.state = {
+            username: '',
+            password: '',
+            remember: false
+        }
     }
-  
+
     inputHandler = (event) => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-  
-      this.setState({
-        [name]: value,
-      });
-    };
-  
+        const name = event.target.name;
+        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+        this.setState ({
+            [name] : value
+        })
+    }
+
     onLogin = () => {
         console.log (this.state);
     }
-  
-    render() {
-      return (
-        <>
-          <h3>Login.js Input:</h3>
-          <input
-            name="username"
-            value={this.state.username}
-            onChange={this.inputHandler}
-            placeholder="username"
-          />
-          <input
-            name="password"
-            value={this.state.password}
-            onChange={this.inputHandler}
-            placeholder="password"
-          />
-          <input
-            name="remember"
-            type="checkbox"
-            checked={this.state.remember}
-            onChange={this.inputHandler}
-          />
-          <button onClick={this.onLogin} disabled={this.state.username !== "" && this.state.password !== "" ? false : true}>Submit</button>
-        </>
-      );
+
+    render () {
+        return (
+            <>
+                <h3>Login.js form:</h3>
+                <input name="username" value={this.state.username} onChange={this.inputHandler} /> 
+                <input name="password" type="password" value={this.state.password} onChange={this.inputHandler} />   
+                <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.inputHandler} />
+                <button disabled={this.state.username !== '' && this.state.password !== '' ? false : true} onClick={this.onLogin}>Login</button> 
+            </>
+        )
     }
-  }
+}
