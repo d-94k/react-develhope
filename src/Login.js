23af55a91049ee434ee417404f91da8faef6fcfb
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-21 11:33:28 +0100

    completed exercise

--- src/Login.js
+++ src/Login.js
@@ -32,14 +32,14 @@ export default class Login extends React.Component {
 
     render () {
         return (
-            <>
+            <div style={{backgroundColor: this.state.password.length > 3 ? 'green' : 'red'}}>
                 <h3>Login.js form:</h3>
                 <input name="username" value={this.state.username} onChange={this.inputHandler} /> 
                 <input name="password" type="password" value={this.state.password} onChange={this.inputHandler} />   
                 <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.inputHandler} />
                 <button disabled={this.state.username !== '' && this.state.password !== '' ? false : true} onClick={this.onLogin}>Login</button>
                 <button onClick={this.onReset}>Reset</button>
-            </>
+            </div>
         )
     }
 }
