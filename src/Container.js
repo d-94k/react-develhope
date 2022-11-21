350beeb6b918337fdd99a06f72c95c984cc1851d
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-21 12:32:42 +0100

    completed exercise

--- src/Container.js
+++ src/Container.js
@@ -4,7 +4,7 @@ export default class Container extends React.Component {
     render () {
         return (
             <div style={{backgroundColor: 'white', border: '2px solid red'}}>
-                <div>{this.props.title}</div>
+                {this.props.title}
                 {this.props.children}
             </div>
         )
