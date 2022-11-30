38ad89a9867369c638cbf57e7ee098cb8386fe63
d-94k <86233674+d-94k@users.noreply.github.com>
2022-11-30 18:02:46 +0100

    completed exercise

--- src/FilteredList.js
+++ src/FilteredList.js
@@ -1,36 +1,10 @@
 import { useMemo } from "react";
 
-const FilteredList = () => {
-
-    const list = [
-        {
-            id: 0,
-            name: "A",
-            age: 15
-        },
-        {
-            id: 1,
-            name: "B",
-            age: 17
-        },
-        {
-            id: 2,
-            name: "C",
-            age: 19
-        }
-    ];
-
-    function filter (list) {
-        let newList = list.filter (item => item.age > 18);
-        return newList.map ((item, index) => <p key={index}>{item.name}</p>)
-    };
-
-    const result = useMemo (() => filter (list), [list]);
-
+const FilteredList = ({list = [{id: 0, name: "A", age: 2}, {id: 1, name: "B", age: 20}]}) => {
+    const filteredArr = useMemo (() => list.filter (item => item.age > 18), [list])
     return (
         <>
-        <h2>Filtered list:</h2>
-        <h3>{result}</h3>
+            <div>filtered prop item with the age of: {filteredArr.map ((item, index) => <p key={index}>{item.age}</p>)}</div>
         </>
     );
 }
