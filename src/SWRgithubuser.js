808f8ee03bfec84387865b7159b86056374ab673
d-94k <86233674+d-94k@users.noreply.github.com>
2022-12-01 20:28:37 +0100

    updated exercise

--- src/SWRgithubuser.js
+++ src/SWRgithubuser.js
@@ -1,12 +1,20 @@
 import useSWR from "swr";
 
-const fetcher = url => fetch(url).then((r) => r.json());
+function useGithubswr (username) {
+    const fetcher = url => fetch(url).then((r) => r.json());
+    const { data, error } = useSWR (`https://api.github.com/users/${username}`, fetcher);
+    return {
+        data,
+        error,
+        loading: !data && !error
+    }
+}
 
 const SWRgithubuser = ({ username }) => {
-    const { data, error } = useSWR (`https://api.github.com/users/${username}`, fetcher);
+    const { data, error, loading } = useGithubswr (username);
     return (
         <>
-            {!data && !error && <h3>Loading...</h3>}
+            {loading && <h3>Loading...</h3>}
             {error && <h3>An error has occurred</h3>}   
             {!error && data && <h3>fetched Github user with id #{data.id}</h3>}
         </>
